f9b24158b07cd241221a4f5cb0304dd2dc2d8418
ebratt <eric_bratt@yahoo.com>
2016-03-02 17:37:24 -0600

    included some more samples from the web

--- run.R
+++ run.R
@@ -61,6 +61,174 @@ colnames(data) <- c("species", "thorax", "elytra", "aj2", "aj3")
 data$species_name <- ifelse(data$species == "a", "oleracea","cardourum")
 head(data)
 
+# Look at the profile plot
+makeProfilePlot <- function(mylist,names)
+{
+    # from http://little-book-of-r-for-multivariate-analysis.readthedocs.org/en/latest/src/multivariateanalysis.html
+    require(RColorBrewer)
+    # find out how many variables we want to include
+    numvariables <- length(mylist)
+    # choose 'numvariables' random colours
+    colours <- brewer.pal(numvariables,"Set1")
+    # find out the minimum and maximum values of the variables:
+    mymin <- 1e+20
+    mymax <- 1e-20
+    for (i in 1:numvariables)
+    {
+        vectori <- mylist[[i]]
+        mini <- min(vectori)
+        maxi <- max(vectori)
+        if (mini < mymin) { mymin <- mini }
+        if (maxi > mymax) { mymax <- maxi }
+    }
+    # plot the variables
+    for (i in 1:numvariables)
+    {
+        vectori <- mylist[[i]]
+        namei <- names[i]
+        colouri <- colours[i]
+        if (i == 1) { plot(vectori,col=colouri,type="l",ylim=c(mymin,mymax)) }
+        else         { points(vectori, col=colouri,type="l")                                     }
+        lastxval <- length(vectori)
+        lastyval <- vectori[length(vectori)]
+        text((lastxval-10),(lastyval),namei,col="black",cex=0.6)
+    }
+}
+load_package("RColorBrewer")
+names <- colnames(data[,2:5])
+mylist <- as.list(data[,2:5])
+makeProfilePlot(mylist,names)
+
+# look at means and sd's
+sapply(data[,2:5],mean)
+sapply(data[,2:5],sd)
+# they appear to be consistent when not grouped
+
+# what about grouped by species?
+printMeanAndSdByGroup <- function(variables,groupvariable)
+{
+    # from http://little-book-of-r-for-multivariate-analysis.readthedocs.org/en/latest/src/multivariateanalysis.html
+    # find the names of the variables
+    variablenames <- c(names(groupvariable),names(as.data.frame(variables)))
+    # within each group, find the mean of each variable
+    groupvariable <- groupvariable[,1] # ensures groupvariable is not a list
+    means <- aggregate(as.matrix(variables) ~ groupvariable, FUN = mean)
+    names(means) <- variablenames
+    print(paste("Means:"))
+    print(means)
+    # within each group, find the standard deviation of each variable:
+    sds <- aggregate(as.matrix(variables) ~ groupvariable, FUN = sd)
+    names(sds) <- variablenames
+    print(paste("Standard deviations:"))
+    print(sds)
+    # within each group, find the number of samples:
+    samplesizes <- aggregate(as.matrix(variables) ~ groupvariable, FUN = length)
+    names(samplesizes) <- variablenames
+    print(paste("Sample sizes:"))
+    print(samplesizes)
+}
+printMeanAndSdByGroup(data[2:5],data[1])
+
+# look at within group variances
+calcWithinGroupsVariance <- function(variable,groupvariable)
+{
+    # from http://little-book-of-r-for-multivariate-analysis.readthedocs.org/en/latest/src/multivariateanalysis.html
+    # find out how many values the group variable can take
+    groupvariable2 <- as.factor(groupvariable[[1]])
+    levels <- levels(groupvariable2)
+    numlevels <- length(levels)
+    # get the mean and standard deviation for each group:
+    numtotal <- 0
+    denomtotal <- 0
+    for (i in 1:numlevels)
+    {
+        leveli <- levels[i]
+        levelidata <- variable[groupvariable==leveli,]
+        levelilength <- length(levelidata)
+        # get the standard deviation for group i:
+        sdi <- sd(levelidata)
+        numi <- (levelilength - 1)*(sdi * sdi)
+        denomi <- levelilength
+        numtotal <- numtotal + numi
+        denomtotal <- denomtotal + denomi
+    }
+    # calculate the within-groups variance
+    Vw <- numtotal / (denomtotal - numlevels)
+    return(Vw)
+}
+
+# calculate between group variances
+calcBetweenGroupsVariance <- function(variable,groupvariable)
+{
+    # from http://little-book-of-r-for-multivariate-analysis.readthedocs.org/en/latest/src/multivariateanalysis.html
+    # find out how many values the group variable can take
+    groupvariable2 <- as.factor(groupvariable[[1]])
+    levels <- levels(groupvariable2)
+    numlevels <- length(levels)
+    # calculate the overall grand mean:
+    grandmean <- mean(variable[[1]])
+    # get the mean and standard deviation for each group:
+    numtotal <- 0
+    denomtotal <- 0
+    for (i in 1:numlevels)
+    {
+        leveli <- levels[i]
+        levelidata <- variable[groupvariable==leveli,]
+        levelilength <- length(levelidata)
+        # get the mean and standard deviation for group i:
+        meani <- mean(levelidata)
+        sdi <- sd(levelidata)
+        numi <- levelilength * ((meani - grandmean)^2)
+        denomi <- levelilength
+        numtotal <- numtotal + numi
+        denomtotal <- denomtotal + denomi
+    }
+    # calculate the between-groups variance
+    Vb <- numtotal / (numlevels - 1)
+    Vb <- Vb[[1]]
+    return(Vb)
+}
+
+# separation is the between-group variance divided by the within-group variance
+calcSeparations <- function(variables,groupvariable)
+{
+    # find out how many variables we have
+    variables <- as.data.frame(variables)
+    numvariables <- length(variables)
+    # find the variable names
+    variablenames <- colnames(variables)
+    # calculate the separation for each variable
+    for (i in 1:numvariables)
+    {
+        variablei <- variables[i]
+        variablename <- variablenames[i]
+        Vw <- calcWithinGroupsVariance(variablei, groupvariable)
+        Vb <- calcBetweenGroupsVariance(variablei, groupvariable)
+        sep <- Vb/Vw
+        print(paste("variable",variablename,"Vw=",Vw,"Vb=",Vb,"separation=",sep))
+    }
+}
+calcSeparations(data[2:5],data[1])
+
+# find the most highly-correlated variables
+mosthighlycorrelated <- function(mydataframe,numtoreport)
+{
+    # find the correlations
+    cormatrix <- cor(mydataframe)
+    # set the correlations on the diagonal or lower triangle to zero,
+    # so they will not be reported as the highest ones:
+    diag(cormatrix) <- 0
+    cormatrix[lower.tri(cormatrix)] <- 0
+    # flatten the matrix into a dataframe for easy sorting
+    fm <- as.data.frame(as.table(cormatrix))
+    # assign human-friendly names
+    names(fm) <- c("First.Variable", "Second.Variable","Correlation")
+    # sort and print the top n correlations
+    head(fm[order(abs(fm$Correlation),decreasing=T),],n=numtoreport)
+}
+mosthighlycorrelated(data[2:5], 4)
+cor(data[,2:5])
+
 # scatterplot matrix - no grouping
 load_package("car")
 png(concat(IMAGES_DIR,'/beetle scatterplot 1.png'), 
@@ -117,8 +285,8 @@ load_package("MASS")
 # Box's M: It performs the Box's M-test for homogeneity of covariance matrices 
 # obtained from multivariate normal data according to one classification factor. 
 # The test is based on the chi-square approximation.
-load_package("biotools")
-boxM(data[,2:5], grouping=data$species_name)
+# load_package("biotools")
+# boxM(data[,2:5], grouping=data$species_name)
 # Box's M-test for Homogeneity of Covariance Matrices
 # 
 # data:  data[, -1]
@@ -247,14 +415,16 @@ data[, 2:13] <- scale(x,
                       center=FALSE, 
                       scale=apply(x,2, sd, na.rm=TRUE))
 head(data)
+
+# Performa Linear Descriminant Analysis
 lda <- lda(formula=data$facrank ~ ., data=data)
 ld_coefs <- lda$scaling
 write.table(round(ld_coefs,3), file=concat(OUTPUT_DIR,'/faculty lda coefficients.csv'), sep=",")
 # Box's M: It performs the Box's M-test for homogeneity of covariance matrices 
 # obtained from multivariate normal data according to one classification factor. 
 # The test is based on the chi-square approximation.
-load_package("biotools")
-boxM(data[,2:13], grouping=data[,1])
+# load_package("biotools")
+# boxM(data[,2:13], grouping=data[,1])
 # Box's M test suggests that the variance-covariance matrices are not
 # homegeneous:
 # Box's M-test for Homogeneity of Covariance Matrices
@@ -295,7 +465,7 @@ round(1 - diag(prop.table(confusion_matrix2)), 4)
 round(1 - sum(diag(prop.table(confusion_matrix2))), 4)
 
 # plot the LDA projection
-(prop.lda = lda$svd^2/sum(lda$svd^2))
+prop.lda = lda$svd^2/sum(lda$svd^2)
 lda_data <- data.frame(facrank = data[,"facrank"], lda = plda$x)
 lda_data <- lda_data[,1:3] # drop unnecessary LD's
 load_package("ggplot2")
@@ -314,8 +484,7 @@ dev.off()
 # answer to a.             #
 ############################
 scaled_data <- data[,2:13]
-summary(scaled_data)
-# Determine number of clusters
+# Determine number of clusters based on weighted sum of squares
 wss <- (nrow(scaled_data)-1)*sum(apply(scaled_data,2,var))
 for (i in 2:15) wss[i] <- sum(kmeans(scaled_data, 
                                      centers=i)$withinss)
@@ -384,7 +553,6 @@ plotcluster(x=scaled_data,
             method="dc",
             clnum=clvecd,
             main="Cluster Plot of Faculty Data with k=5")
-legend("topleft", legend = paste(lda$lev), pch=16, col=1:5)
 dev.off()
 png(concat(IMAGES_DIR,'/faculty cluster plot4.png'), 
     width = 1024, height = 1024)
@@ -395,8 +563,6 @@ plotcluster(x=scaled_data,
             main="Cluster Plot of Faculty Data with k=5")
 legend("topleft", legend = paste(lda$lev), pch=16, col=1:5)
 dev.off()
-
-
 
 
 
@@ -443,8 +609,8 @@ lda$scaling
 # Box's M: It performs the Box's M-test for homogeneity of covariance matrices 
 # obtained from multivariate normal data according to one classification factor. 
 # The test is based on the chi-square approximation.
-load_package("biotools")
-boxM(D[,2:3], grouping=D[,1])
+# load_package("biotools")
+# boxM(D[,2:3], grouping=D[,1])
 # Box's M test suggests that the variance-covariance matrices are not
 # homegeneous:
 # Box's M-test for Homogeneity of Covariance Matrices
